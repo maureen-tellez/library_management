7bcd5391431b42f6fbe895d370ce4e9ce65f2ce2
Maureen Tellez <maureentellez0816@gmail.com>
2025-01-23 21:50:31 -0600

    1ra versión

--- Libreria.sql
+++ Libreria.sql
@@ -8,13 +8,6 @@ CREATE TABLE IF NOT EXISTS editorial (
     CONSTRAINT editorial_check_1 CHECK(year_established > 0)
 );
 
--- Create genre table
-CREATE TABLE IF NOT EXISTS genre (
-    id_genre INT NOT NULL,
-    name VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id_genre)
-);
-
 -- Create authors table
 CREATE TABLE IF NOT EXISTS authors (
     id_author INT NOT NULL,
@@ -25,14 +18,6 @@ CREATE TABLE IF NOT EXISTS authors (
     PRIMARY KEY (id_author)
 );
 
--- Create members table
-CREATE TABLE IF NOT EXISTS members (
-    id_member INT NOT NULL,
-    name VARCHAR(50) NOT NULL,
-    mobile VARCHAR(20) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    PRIMARY KEY (id_member)
-);
 
 -- Create books table
 CREATE TABLE IF NOT EXISTS books (
@@ -48,30 +33,3 @@ CREATE TABLE IF NOT EXISTS books (
     CONSTRAINT author_id_fk FOREIGN KEY (author_id_author) REFERENCES authors (id_author) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
--- Create reviews table
-CREATE TABLE IF NOT EXISTS reviews (
-    review_number INT NOT NULL,
-    review TEXT NOT NULL,
-    book_id_book INT NOT NULL,
-    PRIMARY KEY (review_number),
-    CONSTRAINT book_id_fk FOREIGN KEY (book_id_book) REFERENCES books (id_book) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
--- Create loans table
-CREATE TABLE IF NOT EXISTS loans (
-    book_id_book INT NOT NULL, 
-    member_id_member INT NOT NULL,
-    loan_date TIMESTAMP NOT NULL,
-    return_date DATE DEFAULT NULL,
-    PRIMARY KEY (book_id_book, member_id_member),
-    CONSTRAINT book_id_fk FOREIGN KEY (book_id_book) REFERENCES books (id_book) ON DELETE NO ACTION ON UPDATE NO ACTION, 
-    CONSTRAINT member_id_fk FOREIGN KEY (member_id_member) REFERENCES members (id_member) ON DELETE NO ACTION ON UPDATE NO ACTION,
-    CONSTRAINT loan_return_check CHECK (return_date > loan_date)
-);
-
--- Create book_genres table
-CREATE TABLE IF NOT EXISTS book_genres (
-    book_id_book INT NOT NULL,
-    genre_id_genre INT NOT NULL,
-    PRIMARY KEY (book_id_book, genre_id_genre),
-    FOREIGN KEY (book_id_book) REFERENCES books(id_book) ON DELETE NO ACTION ON UPDA
